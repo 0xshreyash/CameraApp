a72e7b777ae34e856cca0535cde33d4948409e7e
ShreyashPatodia <patodiashreyash32@gmail.com>
2016-10-27 18:56:29 +1100

    Initial Commit

---
+++ CameraApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CameraApp
+//
+//  Created by Shreyash Patodia on 27/10/16.
+//  Copyright © 2016 shreyash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
